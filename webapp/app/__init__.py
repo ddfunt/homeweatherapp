e3c22e5edad4c11e54fc45940deae5bb6e9a537b
Matt Muckle <matt.muckle@brightspec.com>
2017-07-08 00:51:55 -0400

    removed unneeded reference to pymongo

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -7,7 +7,7 @@ Licence: GPLv3
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 from flask_login import LoginManager
 
 app = Flask(__name__)
